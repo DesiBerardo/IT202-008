81b3cd393969837309e70b989d48f7da3060cb25
DesiBerardo <db32@njit.edu>
2022-04-15 21:11:01 -0400

    dashboard page added

---
+++ public_html/project/dashboard.php
@@ -0,0 +1,17 @@
+<?php
+require_once(__DIR__ . "/../../partials/nav.php");
+if (!is_logged_in()) {
+    die(header("Location: login.php"));
+}
+?>
+
+<Div>
+    <h3><a href="#">My Accounts</a></h3>
+    <h3><a href="#">Deposit</a></h3>
+    <h3><a href="#">Withdraw</a></h3>
+    <h3><a href="#">Transfer</a></h3>
+    <h3><a href="#">Profile</a></h3>
+</Div>
+
+<?php
+require(__DIR__ . "/../../partials/flash.php");
